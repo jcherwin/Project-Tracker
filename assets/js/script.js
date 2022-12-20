796c341576202a35273d3ffc77277b0443bcc593
Duke Nukem <joncherwin@gmail.com>
2022-12-19 23:43:00 -0600

    Task 2&3 Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
-// save reference to important DOM elements
+// Save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
+
+/* different variables for modal window */
+var projectForm = $("#projForm");
+var projectName = $("#projName");
+var projectType = $("#projType");
+var projectDueDate = $("#datepicker");
+var projectSubmit = $("#projSubmit");
+var projectModal = $("#myModal");
+var tableBody = $("#tableBody");
+
+/* global variables to place values from modal window */
+var projName;
+var projType;
+var projDueDate;
 
 var timer = {
   start(){
-    this.timeoutID = setInterval(() => {
-      displayTime();
-    },1000);
+    setInterval(() => { displayTime(); }, 1000);
   },
 }
 
-// handle displaying the time
+// Handle displaying the time
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
@@ -32,6 +44,49 @@ function saveProjectsToStorage(projects) {
   localStorage.setItem('projects', JSON.stringify(projects));
 }
 
-// ADDING MY ADDITIONS HERE
-timer.start();
-displayTime();
+function captureFormData(){
+  projName = projectName.val();
+  projType = projectType.val();
+  projDueDate = projectDueDate.val();
+  var projectItem = [projName, projType, projDueDate];
+  var projectSet = readProjectsFromStorage();
+  projectSet.push(projectItem);
+  saveProjectsToStorage(projectSet);
+}
+
+function printProjectData(){
+  tableBody.text('');
+  // If the project is past due, give the row a class so that the row for the project will have a light red background. 
+  // If the project is due today, give the row a class so that the row will have a light yellow background.
+  var projectSet = readProjectsFromStorage();
+  for(var x in projectSet){  //goes over each array (1 table row each)
+    var tableRow = $('<tr>');
+    for(var y in projectSet[x]){  //goes through the contents of each array (fill out each table row)
+      var tableData = $('<td>');
+      tableData.text(projectSet[x][y]);
+      //console.log(projectSet[x][y]);
+      tableRow.append(tableData);
+    }
+    tableRow.append();
+    tableBody.append(tableRow);
+  }
+}
+
+// Apply jQueryUI datepicker to modal input
+$( function() {
+  projectDueDate.datepicker();
+  displayTime();
+  timer.start();
+  printProjectData();
+} );
+
+// Add Event Listeners here
+projectSubmit.on("click", captureFormData);
+
+projectModal.on("hidden.bs.modal", function() {
+  //Adds a short timer to form reset so submit has time to capture data
+  setTimeout(() => {
+    projectForm[0].reset();
+    printProjectData(); 
+  }, 500);
+});
