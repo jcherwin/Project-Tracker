f3c6a3a087322550f9b76a5c338d92eff00f824f
J03B <byucrazyfan@gmail.com>
2022-12-19 20:12:26 -0600

    Added starter code

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// save reference to important DOM elements
+var timeDisplayEl = $('#time-display');
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+// Reads projects from local storage and returns array of project objects.
+// Returns an empty array ([]) if there aren't any projects.
+function readProjectsFromStorage() {
+  var projects = localStorage.getItem('projects');
+  if (projects) {
+    projects = JSON.parse(projects);
+  } else {
+    projects = [];
+  }
+  return projects;
+}
+
+// Takes an array of projects and saves them in localStorage.
+function saveProjectsToStorage(projects) {
+  localStorage.setItem('projects', JSON.stringify(projects));
+}
+
+// ADDING MY ADDITIONS HERE
+displayTime();
